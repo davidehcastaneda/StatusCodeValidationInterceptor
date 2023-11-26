7949dddc65b3e4a02c18265d9e1b85a69c79a3cd
David Castaneda <davidehcastaneda@gmail.com>
2023-11-25 22:09:59 -0500

    Adds base validation interceptor implementation

--- Package.swift
+++ Package.swift
@@ -5,17 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "StatusCodeValidationInterceptor",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "StatusCodeValidationInterceptor",
             targets: ["StatusCodeValidationInterceptor"]),
     ],
+    dependencies: [
+        // Add your package dependencies here
+        .package(url: "https://github.com/davidehcastaneda/NetworkClient", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "StatusCodeValidationInterceptor"),
+            name: "StatusCodeValidationInterceptor",
+            dependencies: ["NetworkClient"]),
         .testTarget(
             name: "StatusCodeValidationInterceptorTests",
             dependencies: ["StatusCodeValidationInterceptor"]),
--- Sources/StatusCodeValidationInterceptor/StatusCodeValidationInterceptor.swift
+++ Sources/StatusCodeValidationInterceptor/StatusCodeValidationInterceptor.swift
@@ -1,2 +1,25 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import NetworkClient
+import Foundation
+
+class StatusCodeValidationInterceptor: Interceptor {
+    func intercept(request: Request, chain: Chain) async throws -> (Data, URLResponse) {
+        let response = try await chain.proceed(request: request)
+        try validateHttpStatusCodes(from: response.1)
+        return response
+    }
+
+    private func validateHttpStatusCodes(from response: URLResponse) throws {
+        guard let response = response as? HTTPURLResponse else { throw ResponseValidatorInterceptorErrors.responseIsNotHttp }
+        let statusCode = response.statusCode
+        guard statusCode >= 200 else { throw ResponseValidatorInterceptorErrors.noInfoAvailable(response) }
+        guard statusCode < 500 else { throw ResponseValidatorInterceptorErrors.apiError(response) }
+        guard statusCode < 400 else { throw ResponseValidatorInterceptorErrors.requestError(response) }
+    }
+
+    private enum ResponseValidatorInterceptorErrors: Error {
+        case responseIsNotHttp
+        case noInfoAvailable(CustomStringConvertible)
+        case apiError(CustomStringConvertible)
+        case requestError(CustomStringConvertible)
+    }
+}
